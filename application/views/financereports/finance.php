b8fd5a7129ce122b3459f1b11da110d0c3d5de50
atishdip07 <atishdip07@gmail.com>
2025-04-18 22:10:57 +0530

    Initial commit

---
+++ application/views/financereports/finance.php
@@ -0,0 +1,12 @@
+<?php
+$currency_symbol = $this->customlib->getSchoolCurrencyFormat();
+?>
+<div class="content-wrapper col-md-10 float-right">    
+    <!-- Main content -->
+    <section class="content">
+        <?php $this->load->view('financereports/_finance'); ?>
+
+</div>   
+</div>  
+</section>
+</div>
